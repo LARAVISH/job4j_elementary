e5df1656a086e0c37d9696d89d9036e6778109a6
LARAVISH <fandvor@mail.ru>
2020-10-27 17:03:18 +0500

    checkstyle.xml add new rule

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "Я люблю Java!";
+        System.out.println(idea );
+        idea = "Я люблю Java! Но я новичок";
+        int year = 2020;
+        idea = idea + " " +  year ;
+        System.out.println(idea );
+    }
+}
--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -3,14 +3,13 @@ package ru.job4j.condition;
 public class Point {
 
     public static double distance (int x1, int y1, int x2, int y2){
-        double res = Math.pow((x2 - x1),2);
-        double res1 = Math.pow((y2 - y1),2);
-        double result = Math.sqrt((res + res1));
-        return result;
+
+
+        return Math.sqrt(Math.pow(((x2 - x1) + (y2 - y1)),2));
     }
 
     public static void main(String[] args) {
-        double res = Point.distance(0,0,2,0);
+        double res = Point.distance(4,0,8,0);
         System.out.println("result (0,0) (2,0) " + res);
     }
 
